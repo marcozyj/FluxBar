2d1d351e742a886497218e82138ce81ab9627310
noah <1725276230@qq.com>
2026-03-13 20:53:36 +0800

    Initial Commit

---
+++ FluxBar/FluxBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxBarApp.swift
+//  FluxBar
+//
+//  Created by noah on 2026/3/13.
+//
+
+import SwiftUI
+
+@main
+struct FluxBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
